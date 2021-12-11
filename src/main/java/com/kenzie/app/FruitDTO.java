129a975ec95fded8471c27e6fdb317b7324fb93f
Behzod Mamadiev <bmamadiev@gmail.com>
2021-12-11 17:24:23 -0500

    Input-Output Assessment

--- src/main/java/com/kenzie/app/FruitDTO.java
+++ src/main/java/com/kenzie/app/FruitDTO.java
@@ -1,14 +1,147 @@
 package com.kenzie.app;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({"genus", "name", "id", "family", "order", "nutritions"})
 public class FruitDTO {
+    @JsonProperty("genus")
+    private String genus;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("family")
+    private String family;
+    @JsonProperty("order")
+    private String order;
+    @JsonProperty("nutritions")
+    private Nutritions nutritions;
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public Nutritions getNutritions() {
+        return nutritions;
+    }
+
+    public void setNutritions(Nutritions nutritions) {
+        this.nutritions = nutritions;
+    }
+
+    @JsonPropertyOrder({"carbohydrates", "protein", "fat", "calories", "sugar"})
+    public static class Nutritions {
+        @JsonProperty("carbohydrates")
+        private double carbohydrates;
+        @JsonProperty("protein")
+        private double protein;
+        @JsonProperty("fat")
+        private double fat;
+        @JsonProperty("calories")
+        private double calories;
+        @JsonProperty("sugar")
+        private double sugar;
+
+        public double getCarbohydrates() {
+            return carbohydrates;
+        }
+
+        public void setCarbohydrates(double carbohydrates) {
+            this.carbohydrates = carbohydrates;
+        }
+
+        public double getProtein() {
+            return protein;
+        }
+
+        public void setProtein(double protein) {
+            this.protein = protein;
+        }
+
+        public double getFat() {
+            return fat;
+        }
+
+        public void setFat(double fat) {
+            this.fat = fat;
+        }
+
+        public double getCalories() {
+            return calories;
+        }
+
+        public void setCalories(double calories) {
+            this.calories = calories;
+        }
+
+        public double getSugar() {
+            return sugar;
+        }
+
+        public void setSugar(double sugar) {
+            this.sugar = sugar;
+        }
+
+        @Override
+        public String toString() {
+            return "Nutritions{" +
+                    "carbohydrates=" + carbohydrates +
+                    ", protein=" + protein +
+                    ", fat=" + fat +
+                    ", calories=" + calories +
+                    ", sugar=" + sugar +
+                    '}';
+        }
+    }
     // TODO: use DTO Generator to fill in getters and setters for FruitDTO and Nutritions
     // make sure to remove "abstract" from the FruitDTO class definition
 
-    @JsonPropertyOrder({"carbohydrates", "protein", "fat", "calories", "sugar"})
-    static class Nutritions {
-
+    @Override
+    public String toString() {
+        return "FruitDTO{" +
+                "genus='" + genus + '\'' +
+                ", name='" + name + '\'' +
+                ", id=" + id +
+                ", family='" + family + '\'' +
+                ", order='" + order + '\'' +
+                ", nutritions=" + nutritions.toString() +
+                '}';
     }
 }
--- src/main/java/com/kenzie/app/FruitHttpClient.java
+++ src/main/java/com/kenzie/app/FruitHttpClient.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -19,42 +20,106 @@ public class FruitHttpClient {
         switch(fc){
             case ALL:
                 // look through the documentation to determine the correct URL
-                return "";
+                return "https://www.fruityvice.com/api/fruit/all";
             case LOW_CARB:
                 // look through the documentation to determine the correct URL
-                return "";
+                return "https://fruityvice.com//api/fruit/carbohydrates?min=0&max=5";
             case HIGH_CALORIE:
                 // look through the documentation to determine the correct URL
-                return "";
+                return "https://fruityvice.com//api/fruit/calories?min=100&max=1000";
             default:
-                return "";
+                return "https://www.fruityvice.com/api/fruit/all";
         }
     }
 
     public String getFruits(String url) {
         // TODO: create a URL object from the desired URL
         // TODO: make the GET request and return the list of FruitDTOs
-        return "";
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 200) {
+                return httpResponse.body();
+            } else {
+                return String.format("GET request failed: %d status code received", statusCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
     }
 
     public static List<FruitDTO> getFruitsList(String httpResponseBody) throws JsonProcessingException {
-        return new ArrayList<FruitDTO>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<FruitDTO>> typeReferenceListFruitDTO =  new TypeReference<>(){};
+
+        List<FruitDTO> fruitList = objectMapper.readValue(httpResponseBody, typeReferenceListFruitDTO);
+
+        return fruitList;
     }
     
         public static FruitDTO createFruitDTO(){
         // TODO: create a sample FruitDTO object based on what you want to send in your PUT request
         // instantiate a new FruitDTO object and a new Nutritions DTO object
         // then fill in the properties for each item with the setter methods
-        return new FruitDTO();
+
+            FruitDTO fruitsDTO = new FruitDTO();
+            FruitDTO.Nutritions nutrition = new FruitDTO.Nutritions();
+
+            fruitsDTO.setGenus("Fragaria");
+            fruitsDTO.setName("Behi");
+            fruitsDTO.setId(101);
+            fruitsDTO.setFamily("Rosaceae");
+            fruitsDTO.setOrder("Rosales");
+            fruitsDTO.setNutritions(nutrition);
+
+            nutrition.setCarbohydrates(5.5);
+            nutrition.setProtein(0);
+            nutrition.setFat(0.4);
+            nutrition.setCalories(29);
+            nutrition.setSugar(5.4);
+
+            return fruitsDTO;
     }
 
     public static String fruitToJSON(FruitDTO fruit) throws JsonProcessingException {
         // TODO: convert FruitDTO to JSON using the Jackson library
-        return "";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        return objectMapper.writeValueAsString(fruit);
     }
 
     public String putFruit(String fruitStr, String url) {
         // TODO: make a PUT request with the data, and return the response
-        return "";
+
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(fruitStr))
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 201 || statusCode == 202) {
+                return httpResponse.body();
+            } else {
+                return String.format("GET request failed: %d status code received", statusCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
     }
 }
